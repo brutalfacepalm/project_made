b1ef809805e6b53c5a9251e97a1d452829960e33
Ubuntu <ubuntu@ip-172-31-23-125.us-east-2.compute.internal>
2021-10-27 20:17:48 +0000

    Changes to be committed: 	modified:   .gitignore 	modified:   docker-compose.yml 	modified:   services/flask_app/model.py 	modified:   services/nginx/nginx.conf
     Untracked files:
    	.env.dev
    	git_command.sh

--- services/flask_app/model.py
+++ services/flask_app/model.py
@@ -4,11 +4,10 @@ import boto3
 def load_files_from_s3():
     s3_resource = boto3.resource(
         "s3",
-        aws_access_key_id="AKIATSAITSQ32PPLS6FV",
-        aws_secret_access_key="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",
+        aws_access_key_id="AKIATSAITSQ35WWPFWKJ",
+        aws_secret_access_key="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",
         region_name="us-east-2"
     )
-    # s3 = boto3.client('s3', aws_access_key_id='AKIATSAITSQ32PPLS6FV', aws_secret_access_key='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')
     bucket = s3_resource.Bucket(name='made-classifier-food-type')
 
     bucket.download_file('avg_claims_model.pkl', 'avg_claims_model.pkl')
